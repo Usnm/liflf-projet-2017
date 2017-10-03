dd4f17fe2786bcaac4ae0bd6740c90734f7ce643
Jayson KACED <jayson.kaced@gmail.com>
2017-10-03 14:09:45 +0200

    Add files via upload

--- src/Accept.cpp
+++ src/Accept.cpp
@@ -34,15 +34,47 @@ etatset_t Delta(const sAutoNDE& at, const etatset_t& e, symb_t c){
 ////////////////////////////////////////////////////////////////////////////////
 
 bool Accept(const sAutoNDE& at, std::string str){
-  //TODO définir cette fonction
+	sAutoNDE at2 = at;
+	bool isFinal = false;
+  	//TODO définir cette fonction
+	//debug
+	std::cout << str << std::endl;
+	std::cout << "trans 0 : " << at.trans << std::endl;
+	std::cout << "finaux : " << at.finaux << std::endl;
+	std::cout << "initial : " << at.initial << std::endl;
 
-  for (int i = 0; i < (int)str.size(); ++i) {
-    std::cout << str[i] << std::endl;
-  }
+	if(str.compare("") == 0){
+		//std::set<etat_t>::iterator it;
+		for (auto f : at.finaux)
+		{
+			if(at.initial == f){
+				isFinal = true;
+			}
+			//std::cout << "finaux2 : " << f << std::endl;
+		}
 
-  std::cout << at.trans.front().front() << std::endl;
-
-  return false;
+		if(isFinal){
+			return true;
+		}else{
+			return false;
+		}
+		//verifier que at est un etat final
+		//si oui alors return true
+		//sino return false
+	}else{
+		//parcour des elements
+		//algo null a changer
+		if(str[0] != 'a'){
+			if(at2.initial == 0){
+				at2.initial = 1;	
+			}else{
+				at2.initial = 0;
+			}
+			
+		}
+		
+		return Accept(at2, str.erase(0,1));
+	}
 }
 
 //******************************************************************************
