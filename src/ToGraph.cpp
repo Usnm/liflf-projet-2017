d4a9c879eadc77c27f11db59bb11ec86aed1c9f2
jayson <jayson.kaced@gmail.com>
2017-12-22 13:50:58 +0100

    ToGraph

--- src/ToGraph.cpp
+++ src/ToGraph.cpp
@@ -1,72 +1,60 @@
-/**
- * \file ToGrap.cpp
- * \brief Générateur de fichier GraphViz
- * \author Jayson KACED
- * \version 1
- *
- * Contient la fonction ToGraph qui permet de générer un fichier GraphViz
- * selon les caractéristiques de l'automate d'étude.
- * Ce fichier .gv sera utilisable par la commande dot pour convertir le fichier
- * en une image représentant le graphe avec ses transitions.
- *
- */
+//******************************************************************************
 
 #include "ToGraph.h"
-
 #include <iostream>
 #include <fstream>
 
 using namespace std;
 
 
-/*!
- * \brief Fonction de génération de fichier GraphViz (.gv)
- *
- * \param at Automate d'étude
- * \param path Chemin du fichier .gv
- * \return VRAI si génération réussie, FAUX sinon.
- */
-bool ToGraph(sAutoNDE& at, string path){
 
-    //TODO définir cette fonction
-    ofstream file(path, ios::out | ios::trunc);
-    if(file) {
-        file << "digraph finite_state_machine {" << endl;
-        file << "\trankdir=LR;" << endl;
-        file << "\tsize=\"10,10\"" << endl << endl;
+////////////////////////////////////////////////////////////////////////////////
 
-        file << "\tnode [shape = doublecircle];";
-        for(etatset_t::iterator it=at.finaux.begin(); it != at.finaux.end(); it++){
-            file << *it << " ";
+bool ToGraph(sAutoNDE& at, std::string path){
+  //TODO définir cette fonction
+        ofstream fichier(path.c_str(), ios::out | ios::trunc);
+            if(fichier){
+
+        fichier <<"digraph finite_state_machine {"<< endl;
+        fichier <<" rankdir=LR;"<< endl;
+        fichier <<" size=\"10,10\"" <<endl<<endl;
+
+        fichier <<" node[shape = doublecircle];";
+        for(etatset_t::iterator it=at.finaux.begin();it!=at.finaux.end();it++)
+        {
+            fichier << (*it) << " " ;
         }
-        file << "\tnode [shape = point ]; q;" << endl;
-        file << "\tnode [shape = circle];" << endl << endl;
+        fichier << endl;
 
-        file << "\tq -> " << at.initial << endl;
+        fichier <<" node[shape = point]; q;" <<endl;
+        fichier <<" node[shape = circle];" <<endl <<endl;
+        fichier <<" q -> "<< at.initial<<";" <<endl;
 
-        for(size_t i = 0; i <= at.trans.size(); i++){
-            for(size_t j = 0; j <= at.trans[i].size(); j++){
-                for(etatset_t::iterator it = at.trans[i][j].begin(); it != at.trans[i][j].end(); it++)
-                    file << "\t" << i << " -> " << *it << " [label = \"" << (j+ASCII_A) << "\"];" << endl;
+        for(size_t i=0 ; i<at.trans.size();i++){
+            for(size_t j=0 ; j<at.trans[i].size();j++){
+                for(etatset_t::iterator iti=at.trans[i][j].begin(); iti!=at.trans[i][j].end();iti++){
+                    fichier<<"  "<< i << " -> " << *iti <<" " << "[label=\"" << (char)(j+ASCII_A)<< "\"];" <<endl;
+                }
             }
         }
-        file << endl;
 
-        for(size_t i = 0; i <= at.epsilon.size(); i++) {
-            for(etatset_t::iterator it = at.epsilon[i].begin(); it != at.epsilon[i].end(); it++)
-                file << "\t" << i << " -> " << *it << " [label = \"ε\"];" << endl;
+        for(size_t i=0;i<at.epsilon.size(); i++){
+            for(etatset_t::iterator iti=at.epsilon[i].begin(); iti !=at.epsilon[i].end(); iti++){
+                fichier<< " \t" << i << "->" << *iti<<" " << "[label = \"ε\"];" <<endl;
+            }
         }
-        file << endl;
 
-        file << "}";
-        file.close();
+        fichier << "}";
+        fichier.close();
 
-        return true;
-    }
-    else {
-        cout << "Impossible d'ouvrir le fichier" << endl;
+        }
+
+        else {
+            cerr <<"Erreur à l'ouverture ! " <<endl;
+        }
+
         return false;
-    }
-}
+
+        }
 
 //******************************************************************************
